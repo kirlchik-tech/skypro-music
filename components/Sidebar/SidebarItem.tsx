75e6d9351a60e1855456b78e6ffc44f6731d60d2
kirlchik-tech <kirsol20188@gmail.com>
2026-06-26 09:10:22 +0300

    Полная версия проекта: компоненты, стили, шрифты

---
+++ components/Sidebar/SidebarItem.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./SidebarItem.module.css";
+
+export default function Header() {
+  return (
+    <nav className={styles.main__nav}>
+      <div className={styles.nav__logo}>
+        <Link href="/">
+          <Image
+            width={113}
+            height={17}
+            className={styles.logo__image}
+            src="/img/logo.png"
+            alt="logo"
+          />
+        </Link>
+      </div>
+      <div className={styles.nav__burger}>
+        <span className={styles.burger__line}></span>
+        <span className={styles.burger__line}></span>
+        <span className={styles.burger__line}></span>
+      </div>
+      <div className={styles.nav__menu}>
+        <ul className={styles.menu__list}>
+          <li className={styles.menu__item}>
+            <Link href="/" className={styles.menu__link}>
+              Главное
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link href="/playlist" className={styles.menu__link}>
+              Мой плейлист
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link href="/auth/signin" className={styles.menu__link}>
+              Войти
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
